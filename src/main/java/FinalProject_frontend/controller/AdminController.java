95254c6d2c000ba2fb2c854dd1e9f8c7269cbdc0
cl_yim05 <101900770+Chilam-Yim@users.noreply.github.com>
2023-08-09 23:50:27 -0400

    Update AdminController.java

--- src/main/java/FinalProject_frontend/controller/AdminController.java
+++ src/main/java/FinalProject_frontend/controller/AdminController.java
@@ -1,7 +1,11 @@
 package FinalProject_frontend.controller;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.EnumSet;
+import java.util.List;
 
+import FinalProject_frontend.model.Item;
 import FinalProject_frontend.model.dto.DistributionCenterDto;
 
 import org.springframework.stereotype.Controller;
@@ -32,6 +36,40 @@ public class AdminController {
     @ModelAttribute
     public void getCenters(Model model) {
         var centers = Arrays.asList(restTemplate.getForObject("http://localhost:8081/api/center", DistributionCenterDto[].class));
+
         model.addAttribute("centers", centers);
+
     }
+
+    @ModelAttribute
+    public void brands(Model model) {
+        var brands = EnumSet.allOf(Item.Brand.class);
+        model.addAttribute("brands", brands);
+    }
+
+    @PostMapping("/search")
+    public String searchCenters(@RequestParam(required = false) String name,
+                                @RequestParam(required = false) String brand,
+                                Model model) {
+        var centers = Arrays.asList(restTemplate.getForObject("http://localhost:8081/api/center", DistributionCenterDto[].class));
+        List<DistributionCenterDto> filteredCenters = new ArrayList<>(centers);
+
+        if (name != null && brand != null) {
+            String brandUpperCase = brand.toUpperCase();
+            filteredCenters.removeIf(center -> center.getItems().stream()
+                    .noneMatch(item -> item.getName().equalsIgnoreCase(name) && item.getBrandFrom() == Item.Brand.valueOf(brandUpperCase)));
+        }
+
+        model.addAttribute("centers", filteredCenters);
+        model.addAttribute("brands", EnumSet.allOf(Item.Brand.class));
+        model.addAttribute("searchName", name);
+        model.addAttribute("searchBrand", brand);
+
+        return "admin";
+    }
+
+
+
+
+
 }
